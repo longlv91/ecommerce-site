6f8662041b1a44cb46bb4f263bdc18e05c3f6edb
Long Le <levanlong.91@gmail.com>
2022-10-13 21:07:47 +0700

    Update content type

--- backend-strapi/src/api/order/controllers/order.js
+++ backend-strapi/src/api/order/controllers/order.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  payment controller
+ *  order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::payment.payment');
+module.exports = createCoreController('api::order.order');
--- backend-strapi/src/api/order/services/order.js
+++ backend-strapi/src/api/order/services/order.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * payment service.
+ * order service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::payment.payment');
+module.exports = createCoreService('api::order.order');
